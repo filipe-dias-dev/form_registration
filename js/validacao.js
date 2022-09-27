d39e63946767530cdf46154da5394c78fe20eaf7
filipe <filipe.dias09@hotmail.com>
2022-09-27 17:18:27 -0300

    mensagens de erro

--- js/validacao.js
+++ js/validacao.js
@@ -4,6 +4,31 @@ export function valida(input) {
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input)
     }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+    }
+}
+
+const mensagensDeErro = {
+    nome: {
+
+    valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo nome não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter pelo menos uma letra, pelo menos um número, e ser maior que seis charaters.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
 }
 
 const validadores = {
