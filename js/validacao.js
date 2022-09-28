5f07d8a646c4bcc00a10a7168e9f5afb973a1332
filipe <filipe.dias09@hotmail.com>
2022-09-28 18:57:41 -0300

    condição da função checa cpf

--- js/validacao.js
+++ js/validacao.js
@@ -76,5 +76,33 @@ function validaCPF(input) {
     const cpfRenvoado = input.value.replace(/\D/g,'')
     let mensagem = ''
 
+    if(!checaCPFRepetido(cpfRenvoado)) {
+        mensagem = 'O CPF digitado não é valido.'
+    }
+
     input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf) {
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999',
+    ]
+    let cpfValido = true
+    
+    valoresRepetidos.forEach(valor => {
+        if(valor == cpf) {
+            cpfValido = false
+        }
+    })
+
+    return cpfValido
 }
